e3b100332d0a1f5b877d67863f7fbe1a09714228
Mansheel Kaur Randhawa <mansheel.kaur.randhawa@pwc.com>
2023-08-17 15:19:36 +0800

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel) 
+        {
+            // return View("Index");
+            dogs.Add(dogViewModel);
+            // Redirects to Index() function
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
